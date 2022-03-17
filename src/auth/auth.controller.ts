b3ea3ebae4f74889d55fdd43a8d96830897a1e1c
Harry Nhu <harry.nhu@vmodev.com>
2022-03-18 00:37:04 +0700

    fix: update signup logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,13 +5,12 @@ import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   @Post('signup')
   signup(@Body() dto: AuthDto,) {
-    console.log(dto);
-    
-    return this.authService.signup();
+    // console.log(dto);
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,47 @@
 /* eslint-disable prettier/prettier */
+import { AuthDto } from './dto/auth.dto';
 import { PrismaService } from './../prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {
-  
+  constructor(private prisma: PrismaService) { }
+  async signup(dto: AuthDto) {
+    // Thực hiện generate password hash
+    const hash = await argon.hash(dto.password);
+    // Thực hiện lưu vào database
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        }
+      })
+      // Thực hiện return user
+      console.log(user);
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // throw new BadRequestException('Email đã tồn tại');
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email đã tồn tại trong hệ thống');
+          // throw new BadRequestException('Email đã tồn tại');
+        }
+      }
+      throw error;
+    }
   }
+
   signin() {
     return {
       msg: 'I am signed in!',
-    };
-  }
-
-  signup() {
-    
-    return {
-      msg: 'I am signed up!',
     };
   }
 }
